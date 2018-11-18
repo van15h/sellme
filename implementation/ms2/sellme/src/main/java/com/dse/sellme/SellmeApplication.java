28b2a3f62d004524ebca4c34dd3246723ff14b29
van15h <vansolo@posteo.net>
2018-11-18 11:11:25 +0100

    ms2 initial commit

---
+++ implementation/ms2/sellme/src/main/java/com/dse/sellme/SellmeApplication.java
@@ -0,0 +1,12 @@
+package com.dse.sellme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellmeApplication.class, args);
+	}
+}
