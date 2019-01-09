63da01cec190bfbe4dc1606068f5d52eac8a61c2
van15h <vansolo@posteo.net>
2019-01-09 21:45:13 +0100

    added show users advertisements

--- implementation/ms2/src/main/java/com/dse/ms2/service/MS2Controller.java
+++ implementation/ms2/src/main/java/com/dse/ms2/service/MS2Controller.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,11 +22,10 @@ import org.springframework.web.client.RestTemplate;
  * Following endpoints are available:
  *            api/advertisements - list all advertisements
  *            api/advertisements/{id} - show specific advertisement
- *            api/{user_id} - list all advertisements of logged in user
- *            api/{user_id}/advertisements/{id} - list specific advertisement of logged in user
+ *            api/user{user_id}/advertisements - list all advertisements of logged in user
  *            api/create - user creates advertisement
  *            api/update/{id} - user updates advertisement
- *            api/delete/{id} - user delete advertisement
+ *            api/delete/{id} - user deletes advertisement
  */
 @RestController
 public class MS2Controller {
@@ -36,12 +36,12 @@ public class MS2Controller {
   public MS2Controller(){
     this.inMemoryRepository = InMemoryRepository.getInstance( new ArrayList<>() );
     this.gson = new Gson();
-    Advertisement a1 = new Advertisement(2, "first",
-        50, "computer nice", "tel. 473823748");
-    Advertisement a2 = new Advertisement(2, "second",
-        50, "computer nice", "tel. 473823748");
-    inMemoryRepository.createAdvertisement(a1);
-    inMemoryRepository.createAdvertisement(a2);
+
+    // dummy repository initialisation
+    inMemoryRepository.createAdvertisement(new Advertisement(2, "first",
+        50, "computer nice", "tel. 473823748"));
+    inMemoryRepository.createAdvertisement(new Advertisement(1, "second",
+        50, "computer nice", "tel. 473823748"));
   }
 
   /**
@@ -85,33 +85,33 @@ public class MS2Controller {
     return gson.toJson(inMemoryRepository.getAdvertisementById(id));
   }
 
+  @RequestMapping(value = "/api/user{user_id}/advertisements", method = RequestMethod.GET, produces = "application/json")
+  public String getAdvertisements(
+      @PathVariable("userId") int userId
+  ) {
+    return gson.toJson(inMemoryRepository.getUserAdvertisements(userId));
+  }
+
+  /**
+   * /api/create URI to create advertisement for valid user
+   * @param userId
+   * @param token
+   * @param advertisement
+   * @return
+   */
   @RequestMapping(value = "/api/create", method = RequestMethod.POST)
   public @ResponseBody ResponseEntity createAdvertisement (
       @RequestParam(value = "userId") int userId,
-      @RequestParam(value = "token") String token
+      @RequestParam(value = "token") String token,
+      @RequestBody Advertisement advertisement
       )
   {
     if (isValid(token)) {
-      Advertisement a3 = new Advertisement(userId, "second",
-        50, "computer nice", "tel. 473823748");
-      inMemoryRepository.createAdvertisement(a3);
+      //Advertisement a3 = new Advertisement(userId, "second", 50, "computer nice", "tel. 473823748");
+      inMemoryRepository.createAdvertisement(advertisement);
       return new ResponseEntity(HttpStatus.OK);
     }
     return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-  }
-
-  private boolean isValid(String token){
-    RestTemplate rt = new RestTemplate();
-    return rt.getForObject(Environment.MS1+"/token/" + token, Boolean.class);
-  }
-
-  private boolean userHasAdvertisement(int id){
-    for (Advertisement adv:
-    inMemoryRepository.getAdvertisements()) {
-      if (adv.getId() == id)
-        return true;
-    }
-    return false;
   }
 
   @RequestMapping(value = "/api/delete/{id}", method = RequestMethod.DELETE)
@@ -130,13 +130,35 @@ public class MS2Controller {
     return new ResponseEntity(HttpStatus.UNAUTHORIZED);
   }
 
-  //TODO
   @RequestMapping(value = "/api/update/{id}", method = RequestMethod.PUT)
-  public String updateAdvertisement (
-      @PathVariable("id") int id
+  public @ResponseBody ResponseEntity updateAdvertisement (
+      @PathVariable("id") int id,
+      @RequestParam(value = "token") String token,
+      @RequestBody Advertisement advertisement
   )
   {
-    return gson.toJson(inMemoryRepository.getAdvertisementById(id));
+    if (isValid(token)) {
+      if (userHasAdvertisement(id)) {
+        inMemoryRepository.updateAdvertisement(advertisement);
+        return new ResponseEntity(HttpStatus.OK);
+      }
+      return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+  }
+
+  private boolean isValid(String token){
+    RestTemplate rt = new RestTemplate();
+    return rt.getForObject(Environment.MS1+"/token/" + token, Boolean.class);
+  }
+
+  private boolean userHasAdvertisement(int id){
+    for (Advertisement adv:
+        inMemoryRepository.getAdvertisements()) {
+      if (adv.getId() == id)
+        return true;
+    }
+    return false;
   }
 
 }
